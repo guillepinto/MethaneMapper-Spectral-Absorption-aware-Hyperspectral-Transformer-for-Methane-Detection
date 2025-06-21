02040ea988700eb273f1fc087cc9626c6c25085a
Majinka10 <camachopinto_@hotmail.com>
2025-06-20 21:09:46 -0500

    Add scripts to train with 16171819 and 2020 data

---
+++ methanemapper/plume_box_run_16171819.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python hyper_main.py --dataset_file hyper_seg --pretrained True --output_dir ./exps/box_model --batch_size 1 --num_workers 2 \
+    --train_img_folder data/train/training_data16171819 \
+    --train_ann_file data/annotations_16171819/annotations_16171819.json \
+    --val_img_folder data/train/training_data16171819 \
+    --val_ann_file data/annotations_16171819/annotations_16171819.json \
+    --stats_file_path data/data_stats \
+    --epochs 2 
+
+# python hyper_main.py --hyper_path ./data --dataset_file hyper_seg --pretrained True --output_dir ./exps/box_model --batch_size 12 --num_workers 8
+#CUDA_VISIBLE_DEVICES=2 python hyper_main.py --masks --hyper_path ./data --dataset_file hyper_seg --output_dir ./exps/segm_model --batch_size 12 --num_workers 6 --frozen_weights ./exps/box_model/mAPfix_checkpoint.pth --epoch 100 --lr_drop 15 --use_wandb True
+#CUDA_VISIBLE_DEVICES=2 python hyper_main.py --masks --hyper_path ./data --dataset_file hyper_seg --output_dir ./exps/segm_model --batch_size 12 --num_workers 6 --frozen_weights ./exps/box_model/mAPfix_checkpoint.pth --epoch 100 --lr_drop 15
