bfb4e330186ff5cb521095b4136edb7f12d0e643
Majinka10 <camachopinto_@hotmail.com>
2025-06-21 21:40:56 -0500

    Comment out distributed mode initialization in init_distributed_mode function

--- methanemapper/util/misc.py
+++ methanemapper/util/misc.py
@@ -441,14 +441,14 @@ def init_distributed_mode(args):
 
     args.distributed = True
 
-    torch.cuda.set_device(args.gpu)
-    args.dist_backend = "nccl"
-    print("| distributed init (rank {}): {}".format(args.rank, args.dist_url), flush=True)
-    torch.distributed.init_process_group(
-        backend=args.dist_backend, init_method=args.dist_url, world_size=args.world_size, rank=args.rank
-    )
-    torch.distributed.barrier()
-    setup_for_distributed(args.rank == 0)
+#     torch.cuda.set_device(args.gpu)
+#     args.dist_backend = "nccl"
+#     print("| distributed init (rank {}): {}".format(args.rank, args.dist_url), flush=True)
+#     torch.distributed.init_process_group(
+#         backend=args.dist_backend, init_method=args.dist_url, world_size=args.world_size, rank=args.rank
+#     )
+#     torch.distributed.barrier()
+#     setup_for_distributed(args.rank == 0)
 
 
 @torch.no_grad()
